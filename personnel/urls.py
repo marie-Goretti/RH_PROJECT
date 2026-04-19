44dd0c55487da37b9e7298caa449e57732e26620
goretti <gorettimgboouna@gmail.com>
2026-04-19 18:14:01 +0000

    modification urls

--- personnel/urls.py
+++ personnel/urls.py
@@ -1,8 +1,11 @@
 
 from django.urls import path
 from . import views
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/login/', permanent=False)),
     path('register/', views.register_view, name='register'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
